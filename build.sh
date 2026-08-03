46ef935672bc3743f53c9884cbd8c66ad77d6b6e
ZythTheCode <mauricebmontano@gmail.com>
2026-08-03 10:04:58 +0800

    Add --clear flag to collectstatic in build.sh to purge stale static cache on Render

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --clear
 python manage.py migrate
 
 # Seed initial data only if SEED_DATA environment variable is set to "true"
