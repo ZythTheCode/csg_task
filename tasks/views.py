4791f7ba363b44678ddbba2594ea4ce5abc0cd23
ZythTheCode <mauricebmontano@gmail.com>
2026-08-06 14:59:40 +0800

    Import decouple config in tasks/views.py

--- tasks/views.py
+++ tasks/views.py
@@ -9,6 +9,7 @@ from django.db.models import Q, Case, When, Value, IntegerField
 from django.core.paginator import Paginator
 from django.core.mail import send_mail
 from django.conf import settings as django_settings
+from decouple import config
 from .models import Task, TaskComment, TaskAttachment, TaskHistory, TaskAssignment
 from .forms import TaskForm, TaskProgressForm, CommentForm, AttachmentForm
 from notifications.models import Notification
